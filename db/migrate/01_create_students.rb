47ae64ac7c00ec8d1fd0f166e45774983611373b
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-05-21 21:31:22 +0000

    automatically backed up by learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -2,6 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
-      t.string :genre
+    end #create_table
+  end #change
       
 end
